3481b58b4862bef8cecd33618194b211dadc91c3
Antis <96950582+franiekidos@users.noreply.github.com>
2025-09-16 17:51:17 +0200

    Update install.py

--- pages/install.py
+++ pages/install.py
@@ -49,7 +49,7 @@ class InstallPage(Vertical):
             raise
 
     async def run_install(self):
-    """Run the full installation process for AntisOS."""
+    # """Run the full installation process for AntisOS."""
     try:
         await self.log("Starting AntisOS installation...")
 
